e93ea73be7c55c89b6e47660cfd580a183085e46
woffordlm <lukewofford@gmail.com>
2022-01-07 11:06:50 -0500

    2.7 update

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs)
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-  };
 
-  printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
