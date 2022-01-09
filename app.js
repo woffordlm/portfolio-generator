b378a4f810f951675ca56dbc9f54fd831d2b1a13
woffordlm <lukewofford@gmail.com>
2022-01-09 09:43:58 -0500

    added question feature

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
 
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-  
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
