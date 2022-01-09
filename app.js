2d937c27499f7d90106873665cce68d478414d1d
woffordlm <lukewofford@gmail.com>
2022-01-09 15:50:53 -0500

    9.3.7 progress

--- app.js
+++ app.js
@@ -41,6 +41,24 @@ const promptUser = () => {
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    {
       type: 'input',
       name: 'about',
       message: 'Provide some information about yourself:'
